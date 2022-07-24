6bc0f9b40471a09a241cca025bb7e969d85ab87e
Kaleidiana <kaleidiana63@gmail.com>
2022-07-24 05:11:27 -0700

    the static library libmy.a

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,2 @@
+Solution for the project 0x09-static_libraries
+
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		return (0);
+
+}
+
