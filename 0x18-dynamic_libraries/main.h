ca7ba95a69a3edef9fbd20fc3f5876d6f226f696
Kaleidiana <kaleidiana63@gmail.com>
2022-10-04 07:23:51 -0700

    Create the dynamic library libdynamic.so containing all the functions listed below:

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,4 +1,4 @@
-#ifndef __MAIN__
+#ifndef __MAIN.H__
 #define __MAIN__
 
 int _putchar(char c);
@@ -7,21 +7,19 @@ int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
-int _strlen(char *s);
+int _strlen.c(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *_strcat(char *dest, char *src);
+char *strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *memcpy(char *dest, char b, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int nrandom();
 
-
-#endif /* __MAIN__ */
+#endif /*__MAIN__ */
